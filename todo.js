36a12acf569aff36b1d3b3daa1cdc4dd776000e8
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-23 12:13:10 +1300

    Add help command

--- todo.js
+++ todo.js
@@ -7,6 +7,17 @@ const arg1 = userInputs[3]
 const arg2 = userInputs[4]
 
 switch (cmd) {
+  case 'help':
+    console.table([
+      { command: 'list' },
+      { command: 'add', arg1: 'task text' },
+      { command: 'done', arg1: 'task id' },
+      { command: 'undo', arg1: 'task id' },
+      { command: 'update', arg1: 'task id', arg2: 'new task text' },
+      { command: 'delete', arg1: 'task id' },
+      { command: 'search', arg1: 'task text' },
+    ])
+    break
   case 'list':
     await commands.list()
     break
@@ -30,5 +41,7 @@ switch (cmd) {
     break
 
   default:
-    console.log(`I don't understand that command: ${cmd}`)
+    console.log(
+      `'${cmd}' is not a valid command. Use 'help' to view available commands`
+    )
 }
