cd9b4e5dc0915d02e589210585b43ada513ab42c
Samir <142699675+samir-lekinwala@users.noreply.github.com>
2023-10-23 16:08:09 +1300

    Add instructions/prompts to terminal

--- todo.js
+++ todo.js
@@ -5,24 +5,33 @@ const userInputs = process.argv
 const cmd = userInputs[2]
 
 switch (cmd) {
+  case 'explain':
+    console.log(`After writing ./todo.js, pick one of the following commands: list - allows user to view tasks. done (task id) - allows user to mark chosen task as finished. add ('new task') - allows user to create a new task. update (id, change task) - changes that specific task. search ('partial string') - allows user to find all tasks which include that word.`)
+    break
+
   case 'list':
     await commands.list()
+    console.log(`Commands: list, done, add, update, search and explain`)
     break
   
   case 'done':
     await commands.completeTask(userInputs[3])
+    console.log(`Commands: list, done, add, update, search and explain`)
     break
 
   case 'add':
     await commands.addTask(userInputs[3])
+    console.log(`Commands: list, done, add, update, search and explain`)
     break
 
   case 'update':
     await commands.updateTask(userInputs[3], userInputs[4])
+    console.log(`Commands: list, done, add, update, search and explain`)
     break
 
   case 'search':
-    await commands.searchTask(userInputs[3], userInputs[4])
+    await commands.searchTask(userInputs[3])
+    console.log(`Commands: list, done, add, update, search and explain`)
     break
 
   default:
